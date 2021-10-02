4e9695b6fab13c51bcf8be2cd2300ff1185a3339
safecorners <safeconers.mobile@gmail.com>
2021-10-02 16:09:29 +0900

    initialize project

---
+++ src/main/java/com/safecornerscoffee/resurrection/ResurrectionApplication.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.resurrection;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResurrectionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResurrectionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/safecornerscoffee/resurrection/ResurrectionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.safecornerscoffee.resurrection;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResurrectionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
